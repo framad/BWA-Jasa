14f52b93f2af012288bccac8dbda1446fb0e0998
Febry Ramadhan <febryramadhan285@gmail.com>
2022-01-21 23:06:20 +0700

    adding few controller and setting the route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+//manggil controller landingcontroller
+use App\Http\Controllers\Landing\LandingController;
 
+//manggil controller di controller dashboard
+use App\Http\Controllers\Dashboard\MemberController;
+use App\Http\Controllers\Dashboard\MyOrderController;
+use App\Http\Controllers\Dashboard\ProfileController;
+use App\Http\Controllers\Dashboard\RequestController;
+use App\Http\Controllers\Dashboard\ServiceController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,10 +21,44 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+//bikin route untuk landing pages yang bisa diakses tanpa login
+//kenapa get karena untuk mendapatkan halaman
+//kenapa untuk booking dll ada {id} nya, karena untuk akses halaman booking/detail/detail/booking
+//diperlukan id dari si apa apa yang dimunculkan tersebut
+Route::get('explore', LandingController::class, 'explore')->name('explore.landing');
+Route::get('booking/{id}', LandingController::class, 'booking')->name('booking.landing');
+Route::get('detail/{id}', LandingController::class, 'detail')->name('detail.landing');
+Route::get('detail_booking/{id}', LandingController::class, 'detail_booking')->name('detail.booking.landing');
+Route::resource('/', LandingController::class);
+
+//bikin route untuk dashboard yang hanya bisa diakses jika sudah login
+Route::group(['prefix' => 'member', 'as' => 'member', 'middleware' => ['auth:sanctum', 'verified']],
+function(){
+    //dashboard
+    Route::resource('dashboard', MemberController::class);
+
+    //service
+    Route::resource('service', ServiceController::class);
+
+    //request
+    Route::get('approve_request/{id}', RequestController::class, 'approve')->name('approve_request');
+    Route::resource('request', RequestController::class);
+
+    //my order
+    Route::get('accept/order/{id}', MyOrderController::class, 'accepted')->name('accept.order');
+    Route::get('reject/order/{id}', MyOrderController::class, 'rejected')->name('reject.order');
+    Route::resource('order', MyOrderController::class);
+
+    //profile
+    Route::get('delete_photo', ProfileController::class, 'delete')->name('delete.photo.profile');
+    Route::resource('profile', ProfileController::class);
 });
 
-Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
-    return view('dashboard');
-})->name('dashboard');
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+//     return view('dashboard');
+// })->name('dashboard');
